7ff4d10c5a1c68b0be481e7816b1c3fa2b4c3c37
Mo <moh19951@outlook.com>
2019-12-04 10:18:16 +0100

    Uitwerking van de exercise

---
+++ form.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+
+<html>
+
+<head>
+    <title>(alleen e-mail form)</title>
+
+    <meta charset="utf-8" />
+
+</head>
+
+<body>
+<h2>Contact Form</h2>
+<form class="form">
+    <p class="email">
+        <input type="text" name="email" id="email" placeholder="mail@example.com" />
+        <label for="email">Email</label>
+    </p>
+
+
+
+
+
+    <p class="submit">
+        <input type="submit" value="Send" />
+    </p>
+</form>
+
+</body>
+
+</html>
